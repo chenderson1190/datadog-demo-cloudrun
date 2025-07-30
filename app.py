78ff1da8a755f6949236e046c3ec5fdde978a381
CJ Henderson <chenderson1190@gmail.com>
2025-07-29 20:11:23 -0400

    add logging

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template
+import logging
 import firebase_admin
-## from firebase_admin import firestore
 from google.cloud import firestore
+import ddtrace
 
 app = Flask(__name__)
 firestore_app = firebase_admin.initialize_app()
 db = firestore.Client(database="datadog-demo-db")
 
+logger = logging.getLogger('library_logger')
+logging.basicConfig(filename="/shared-volume/logs/library.log")
+logger.setLevel(logging.INFO)
+
 @app.route("/")
 def index():
+    logger.info("Index page visited...")
     return render_template('index.html')
 
 @app.route("/library")
 def library():
+    logger.info("Retrieving books from database...")
     books = db.collection("books").get()
+    logger.info("Books retrieved.")
     return render_template('library.html', library_list=books)
 
 
